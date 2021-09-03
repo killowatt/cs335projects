4942cd4cbf761c133573c50d61be19209b286457
killowatt <killowatt2213@gmail.com>
2021-09-03 13:37:39 -0400

    Final commit for abundant and sieve of exercise 01

--- abundant/src/Main.java
+++ abundant/src/Main.java
@@ -1,6 +1,47 @@
 public class Main {
+    // Abundant values are greater than 0
+    public static int abundant(int number) {
+        int sum = 0;
+
+        // We loop up to half of number because we know that numbers that multiply by two
+        // to something higher than our input cannot be a part of our properly divisible set
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) // Only properly divisible numbers
+                sum += i;
+        }
+        return sum - number;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Numbers from 1 up to this number (non-inclusive)
+        final int count = 10000;
+
+        // The total running count of abundant numbers
+        int abundantCount = 0;
+
+        // The max abundance value and the corresponding number responsible for that value
+        int maxAbundance = 0;
+        int maxAbundanceIndex = 0;
+
+        // Non-inclusive up to 10,000 ("less than 10,000")
+        for (int i = 0; i < count; i++)
+        {
+            int abundance = abundant(i);
+
+            // If our number is abundant, add one to count
+            if (abundance > 0)
+                abundantCount++;
+
+            // If this number exceeds the current max abundance, update values accordingly
+            // This preserves the lowest number that reaches a max value
+            if (abundance > maxAbundance) {
+                maxAbundance = abundance;
+                maxAbundanceIndex = i;
+            }
+        }
+
+        System.out.printf("There are %d abundant numbers less than 10,000.\n", abundantCount);
+        System.out.printf("The smallest abundant number (less than 10,000) with highest abundance is %d\n",
+                maxAbundanceIndex);
     }
 }
--- sieve/src/Main.java
+++ sieve/src/Main.java
@@ -1,6 +1,40 @@
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Primes up to this number (non-inclusive) are recorded
+        final int count = 10000;
+
+        // Create our array of primes and fill it with true values
+        boolean primes[] = new boolean[count];
+        Arrays.fill(primes, true);
+
+        // Starting from 2 as per sieve algorithm spec
+        int primeCount = 0;
+        for (int i = 2; i < count; i++) {
+            // Skip values that we've already set to false
+            if (!primes[i])
+                continue;
+
+            // Set all multiples of the current number to false, checking array bounds
+            int multiple = i;
+            while (true) {
+                // Next multiple of i
+                multiple += i;
+
+                // If the multiple exceeds our bounds, stop
+                if (multiple >= count)
+                    break;
+
+                // All multiples are set to false, and we know we're in bounds now
+                primes[multiple] = false;
+            }
+
+            // Print out this number since we know its prime, increasing the count as well
+            primeCount++;
+            System.out.printf("%d, ", i);
+        }
+
+        System.out.printf("\nThere are %d primes less than 10,000", primeCount);
     }
 }
