3460f18d9f9ac1f958d9136cf04e68610dac87c8
killowatt <killowatt2213@gmail.com>
2021-11-03 17:34:37 -0400

    Add working image view for gaussianblur

--- gaussianblur/src/Main.java
+++ gaussianblur/src/Main.java
@@ -1,7 +1,35 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+class GaussianBlurImage extends JComponent {
+    private BufferedImage bufferedImage;
+
+    GaussianBlurImage() {
+        super();
+    }
+
+    public void setImage(BufferedImage image) {
+        bufferedImage = image;
+
+        setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+
+        revalidate();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        System.out.println("repaint");
+
+        Graphics2D g2D = (Graphics2D)g;
+        g2D.drawImage(bufferedImage, 0, 0, this);
+    }
+}
 
 class GaussianBlur extends JFrame {
     GaussianBlur() {
@@ -28,11 +56,14 @@ class GaussianBlur extends JFrame {
 
 
 
+
         JPanel controlPanel = new JPanel();
 
         JSlider blurSlider = new JSlider();
 
         JButton resetButton = new JButton("Reset");
+
+        GaussianBlurImage ererfg = new GaussianBlurImage();
 
         controlPanel.add(blurSlider);
         controlPanel.add(resetButton);
@@ -41,12 +72,41 @@ class GaussianBlur extends JFrame {
         blurSlider.setPaintTicks(true);
         blurSlider.setSnapToTicks(true);
 
-        getContentPane().add(controlPanel);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+        contentPane.add(controlPanel);
+        contentPane.add(ererfg);
 
         quitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
+            }
+        });
+
+        final GaussianBlur thf = this;
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        final JFrame thisFrame = this;
+        openFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                FileDialog dialog = new FileDialog(thisFrame, "Choose an image", FileDialog.LOAD);
+                dialog.setVisible(true);
+
+                String fileName = dialog.getDirectory() + dialog.getFile();
+
+                try {
+                    ererfg.setImage(ImageIO.read(new File(fileName)));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+                pack();
             }
         });
 
