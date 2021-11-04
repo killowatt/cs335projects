42211743be1ec2810dd370f426cc3686a9987f84
killowatt <killowatt2213@gmail.com>
2021-11-04 17:33:58 -0400

    Final commit for gaussianblur, add comments

--- gaussianblur/src/GaussianBlurImage.java
+++ gaussianblur/src/GaussianBlurImage.java
@@ -4,81 +4,115 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
 
+// Gaussian Blur Image class that handles blurring an image and painting it on screen
 class GaussianBlurImage extends JComponent {
+    // Whether or not to show the filtered image
     boolean filter = false;
+
+    // Our original image and the blurred version of it
     private BufferedImage image;
     private BufferedImage blurredImage;
 
+    // Default constructor, just calls the JComponent constructor
     GaussianBlurImage() {
         super();
     }
 
+    // Set image method, handles setting our original image to something new and updating our size
     public void setImage(BufferedImage bufferedImage) {
+        // If the image provided is null, just ignore
+        if (bufferedImage == null)
+            return;
+
+        // By default the image should be unfiltered
         filter = false;
 
+        // Our original image must be copied first in our desired pixel format
         image = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2D = image.createGraphics();
         g2D.drawImage(bufferedImage, 0, 0, null);
 
+        // Set the preferred size of this component to the image size
         setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
 
+        // Revalidate, should repaint the widget as well as let the system know this component has resized
         revalidate();
     }
 
+    // Set blur method, handles creating and applying our gaussian blur kernel to our image
     public void setBlur(int radius, int deviation) {
+        // If our image is null or if our radius is zero, ignore
         if (image == null || radius <= 0)
             return;
 
+        // Our kernel size, always odd so that we have a center point in our kernel
         int kernelSize = (2 * radius) + 1;
 
+        // Use our generate kernel method to dynamically create our kernel and use it in our Kernel object
         float[] kernelData = generateKernel(radius, kernelSize, deviation);
         Kernel kernel = new Kernel(kernelSize, kernelSize, kernelData);
 
+        // Set up our convolve operation to use our kernel, ignoring image edges
         ConvolveOp op = new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
 
+        // Filter our image
         blurredImage = op.filter(image, null);
 
+        // Set the image filtered flag
         filter = true;
 
+        // Repaint this component since things have changed
         repaint();
     }
 
     private float[] generateKernel(int radius, int kernelSize, int deviation) {
+        // Based on the link provided in the Exercise 8 spec
         // https://aryamansharda.medium.com/image-filters-gaussian-blur-eb36db6781b1
 
+        // Scale our sigma value with the radius
         double sigma = Math.max(radius / 2.0, deviation);
 
+        // Set up a 1D array for our kernel to be stored
         float[] kernel = new float[kernelSize * kernelSize];
 
+        // Sum value for normalization
         float sum = 0.0f;
 
+        // Iterate through every possible position in the kernel
         for (int x = -radius; x <= radius; x++) {
             for (int y = -radius; y <= radius; y++) {
-                //System.out.println("xy " + x + " " + y);
-
+                // Numerator and denominator for our gaussian equation
                 double exponentNumerator = -(x * x + y * y);
                 double exponentDenominator = (2 * sigma * sigma);
 
+                // Finally acquire the kernel value at this position
                 double eExpression = Math.pow(Math.E, exponentNumerator / exponentDenominator);
                 double kernelValue = eExpression / (2.0 * Math.PI * sigma * sigma);
 
+                // Convert 2D index to 1D and set it in our kernel array, updating our sum
                 int index = kernelSize * (y + radius) + (x + radius);
                 kernel[index] = (float) kernelValue;
+
                 sum += (float) kernelValue;
             }
         }
 
+        // Normalize our kernel using the sum
         for (int i = 0; i < kernel.length; i++) {
             kernel[i] /= sum;
         }
 
+        // Return our kernel as a 1D array
         return kernel;
     }
 
+    // Our paint component method
     @Override
     protected void paintComponent(Graphics g) {
+        // Get a 2D graphics context from our regular one
         Graphics2D g2D = (Graphics2D) g;
 
+        // If our image has yet to be filtered, just draw the original, otherwise draw the filtered one
         if (!filter)
             g2D.drawImage(image, 0, 0, this);
         else
--- gaussianblur/src/Main.java
+++ gaussianblur/src/Main.java
@@ -5,33 +5,44 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/* Assumptions & Known Issues
+ * Dragging the slider around, especially with larger images and larger radius values, can be performance heavy
+ * The standard deviation value has little effect especially at high radius values, spec only says 1.0 - 3.0 for stddev
+ */
+
+// Main GaussianBlur JFrame, handles user controls and layout
 class GaussianBlur extends JFrame {
     GaussianBlur() {
+        // Title our window "Gaussian Blur"
         super("Gaussian Blur");
 
+        // When we click the close button, exit the application
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-
+        // Create our menu bar and corresponding menus and buttons
         JMenuBar menuBar = new JMenuBar();
 
         JMenu fileMenu = new JMenu("File");
-
         JMenuItem openFileButton = new JMenuItem("Open");
         JSeparator fileSeparator = new JSeparator();
         JMenuItem quitButton = new JMenuItem("Quit");
 
+        // Add our file menu buttons to the file menu
         fileMenu.add(openFileButton);
         fileMenu.add(fileSeparator);
         fileMenu.add(quitButton);
 
+        // Add our file menu to the menu bar
         menuBar.add(fileMenu);
 
+        // Set our JFrame's menu bar to the one we just created
         setJMenuBar(menuBar);
 
-
+        // Create our user control panel and corresponding widgets
         JPanel controlPanel = new JPanel();
 
         JLabel radiusLabel = new JLabel("Radius");
@@ -42,31 +53,37 @@ class GaussianBlur extends JFrame {
 
         JButton resetButton = new JButton("Reset");
 
+        // Fix our labels to be aligned on the center
         radiusLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         radiusLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
+        // Create our gaussian blur radius panel, label and slider
         JPanel radiusPanel = new JPanel();
         radiusPanel.setLayout(new BoxLayout(radiusPanel, BoxLayout.Y_AXIS));
         radiusPanel.add(radiusLabel);
         radiusPanel.add(blurSlider);
 
+        // Create our gaussian standard deviation panel, label and slider
         JPanel deviationPanel = new JPanel();
         deviationPanel.setLayout(new BoxLayout(deviationPanel, BoxLayout.Y_AXIS));
         deviationPanel.add(deviationLabel);
         deviationPanel.add(deviationSlider);
 
+        // Add all of our controls to the user control panel
         controlPanel.add(radiusPanel);
         controlPanel.add(deviationPanel);
         controlPanel.add(resetButton);
 
+        // Create our gaussian blur image widget
         GaussianBlurImage blurImage = new GaussianBlurImage();
 
+        // Add our user controls and gaussian blur image widget to the JFrame
         Container contentPane = getContentPane();
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
         contentPane.add(controlPanel);
         contentPane.add(blurImage);
 
-        //
+        // Set up our blur slider parameters
         blurSlider.setMinimum(0);
         blurSlider.setMaximum(10);
         blurSlider.setMinorTickSpacing(1);
@@ -76,6 +93,7 @@ class GaussianBlur extends JFrame {
         blurSlider.setPaintLabels(true);
         blurSlider.setValue(0);
 
+        // Set up a listener to update the gaussian blur image when we drag the radius slider
         blurSlider.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
@@ -83,7 +101,7 @@ class GaussianBlur extends JFrame {
             }
         });
 
-        //
+        // Set up our deviation slider parameters
         deviationSlider.setMinimum(1);
         deviationSlider.setMaximum(3);
         deviationSlider.setMajorTickSpacing(1);
@@ -93,6 +111,7 @@ class GaussianBlur extends JFrame {
         deviationSlider.setPreferredSize(new Dimension(75, 50));
         deviationSlider.setValue(1);
 
+        // Set up a listener to update the gaussian blur image when we drag the deviation slider
         deviationSlider.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
@@ -100,7 +119,7 @@ class GaussianBlur extends JFrame {
             }
         });
 
-        final GaussianBlur thf = this;
+        // Set up our reset button to just reset the slider values back to zero
         resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -109,28 +128,52 @@ class GaussianBlur extends JFrame {
             }
         });
 
+        // Set up our open file button to open up a file dialog and attempt to set the gaussian blur image
         final JFrame thisFrame = this;
         openFileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Create and show our file dialog
                 FileDialog dialog = new FileDialog(thisFrame, "Choose an image", FileDialog.LOAD);
                 dialog.setVisible(true);
 
+                // If the directory or file name are null, assume the user canceled
+                if (dialog.getDirectory() == null || dialog.getFile() == null)
+                    return;
+
+                // Create the full path from the directory and the file name
                 String fileName = dialog.getDirectory() + dialog.getFile();
 
                 try {
+                    // Try to load the file from disk
+                    BufferedImage image = ImageIO.read(new File(fileName));
+
+                    // If we succeeded reading the file but buffered image is null, the file is invalid
+                    // Show an error
+                    if (image == null) {
+                        JOptionPane.showMessageDialog(thisFrame, "Failed to load image!", "Error",
+                                JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }
+
+                    // If we succeeded, set the gaussian blur image
                     blurImage.setImage(ImageIO.read(new File(fileName)));
                 } catch (IOException ex) {
-                    ex.printStackTrace();
+                    // If we failed to read the file from disk, show an error
+                    JOptionPane.showMessageDialog(thisFrame, "Failed to load file!", "Error",
+                            JOptionPane.ERROR_MESSAGE);
                 }
 
+                // Once our new image has been loaded, pack the frame so it automatically resizes
                 pack();
 
+                // Reset pir blur and deviation sliders
                 blurSlider.setValue(0);
                 deviationSlider.setValue(1);
             }
         });
 
+        // Set up our quit button to just call system exit
         quitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -138,6 +181,7 @@ class GaussianBlur extends JFrame {
             }
         });
 
+        // Finally, pack our JFrame, center it on screen and make it visible
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
@@ -146,6 +190,7 @@ class GaussianBlur extends JFrame {
 
 public class Main {
     public static void main(String[] args) {
+        // Simply create our main GaussianBlur JFrame
         GaussianBlur gaussianBlur = new GaussianBlur();
     }
 }
